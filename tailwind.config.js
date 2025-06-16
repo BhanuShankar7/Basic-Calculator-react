f06f0ff512245fc994722b9841357877c2b82bad
BhanuShankar7 <bhanushankarvaddi27@gmail.com>
2025-06-16 21:05:54 +0530

    created calculator component, completed basic functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -9,21 +10,25 @@ export default {
           DEFAULT: "#1A1A2E",
           dark: "#0F0F1E",
         },
+
         deepBlue: {
           light: "#2A3A5E",
           DEFAULT: "#16213E",
           dark: "#0D1729",
         },
+
         mutedBlue: {
           light: "#506486",
           DEFAULT: "#0F3460",
           dark: "#071D39",
         },
+
         crimsonRed: {
           light: "#F27584",
           DEFAULT: "#E94560",
           dark: "#991D38",
         },
+
         lightGrey: {
           light: "#FFFFFF",
           DEFAULT: "#F9F9F9",
@@ -34,3 +39,4 @@ export default {
   },
   plugins: [],
 };
+
